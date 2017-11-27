864b4120dcdb3fc1edbfd07deffbd6738341713a
kidinamoto01 <kidinamoto@qq.com>
2017-11-27 15:58:46 +0800

    first commit

---
+++ src/app.js
@@ -0,0 +1,40 @@
+const IPFS = require('ipfs')
+
+
+function repo () {
+  return 'ipfs/yjs-demo/' + Math.random()
+}
+
+const ipfs = new IPFS({
+  repo: repo(),
+  EXPERIMENTAL: {
+    pubsub: true
+  }
+})
+
+ipfs.once('ready', () => ipfs.id((err, info) => {
+  if (err) { throw err }
+  console.log('IPFS node ready with address ' + info.id)
+}))
+
+const Y = require('yjs')
+require('y-memory')(Y)
+require('y-array')(Y)
+require('y-text')(Y)
+require('y-ipfs-connector')(Y)
+
+Y({
+    db: {
+      name: 'memory'
+    },
+    connector: {
+      name: 'ipfs',
+      room: 'yjs-demo',
+      ipfs: ipfs
+    },
+    share: {
+      textfield: 'Text'
+    }
+  }).then((y) => {
+    y.share.textfield.bind(document.getElementById('textfield'))
+  })
